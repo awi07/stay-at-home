338134bc7797199b6408e6a7c0f432811ecb3c44
awi07 <62935172+awi07@users.noreply.github.com>
2020-04-30 23:13:16 +0200

    add third part

--- scripts.js
+++ scripts.js
@@ -1,11 +1,26 @@
 //Third part:
+console.log("Script file is loaded");
 
 $("h1").html("This is done by <em>JavaScript!</em>");
-$("p").css("border", "1px solid red");
-$("p").css("padding", "30px");
+ 
 $("h2").toggleClass("blue");
 $("h2").toggleClass("blue");
 
+let name = "Adam";
+let age = 34;
+
+function myLittleFunction(){
+    $("main").append('<p>A p is appended in the main');
+    $("p").css("border", "1px solid red");
+    $("p").css("padding", "30px");
+}
+//myLittleFunction();
+
+$("#add").click(myLittleFunction);
+$("#remove").click(function (){
+    $("p:last-child").remove();
+});
 
 
-console.log("Script file is loaded");
+
+
