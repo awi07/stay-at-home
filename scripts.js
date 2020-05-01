5c85a25f6966100c0e60781550a67acc359ff6bc
awi07 <62935172+awi07@users.noreply.github.com>
2020-05-01 15:19:55 +0200

    add final part

--- scripts.js
+++ scripts.js
@@ -10,17 +10,53 @@ let name = "Adam";
 let age = 34;
 
 function myLittleFunction(){
+    //condition: number of ps is smaller than 5
+            if ($("p").length < 5) {
     $("main").append('<p>A p is appended in the main');
+    } else {
+        console.log ("We reached the maximum paragraph number");
+    }
     $("p").css("border", "1px solid red");
     $("p").css("padding", "30px");
 }
-//myLittleFunction();
+
+//myLittleFunction(); = spustí funkci
 
 $("#add").click(myLittleFunction);
 $("#remove").click(function (){
     $("p:last-child").remove();
 });
 
+//Four part
+
+//if (condition that true or false) {
+    //condition is true
+//} else {
+    //condition is false}
+
+let colors = ["orange", "yellow", "blue", "lime", "magenta"];
+
+colors.forEach(function(color){
+    $("#box-container").append('<div class="box"></div>');
+    $(".box:last-child").css("background", color);
+});
+
+for (let i = 0; i < 100; i++) {
+    console.log("I will not cheat the exam");
+}
+
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
 
 
-
+//$("#box-container").append('<div class="box"></div>');
+//$(".box:last-child").css("background", colors [0] );
+//$("#box-container").append('<div class="box"></div>');
+//$(".box:last-child").css("background", colors [1] );
+//$("#box-container").append('<div class="box"></div>');
+//$(".box:last-child").css("background", colors [2] );
+//$("#box-container").append('<div class="box"></div>');
+//$(".box:last-child").css("background", colors [3] );
+//$("#box-container").append('<div class="box"></div>');
+//$(".box:last-child").css("background", colors [4] );
